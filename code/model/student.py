10df5b948db5790645a1d42995b081ff971b604e
lawalton <liwa9544@colorado.edu>
2018-10-29 00:12:24 -0600

    code/model: added Member and Officer subclasses
    Created basic framework for Member and Officer subclasses.
    Class variables, constructors, and getters/setters.

--- code/model/student.py
+++ code/model/student.py
@@ -4,6 +4,54 @@ class Student(object):
         self.year = year
         self.userId = 1 #implement num
 
+    def getName(self):
+        return self.name
+
+    def setName(self, name):
+        self.name = name
+
+    def getYear(self):
+        return self.year
+
+    def setYear(self):
+        self.year = year
+
+class Member(Student):
+    def __init__(self):
+        Student.__init__(self)
+        self.isActiveMember = False
+        self.numEventsAttended = 0
+
+    def getIsActiveMember(self):
+        return self.isActiveMember
+
+    def setIsActiveMember(self, memberStatus):
+        self.isActiveMember = memberStatus
+
+    def getNumEventsAttended(self):
+        return self.getNumEventsAttended
+
+    def setNumEventsAttended(self, num):
+        self.numEventsAttended = num
+
+class Officer(Student):
+    def __init__(self):
+        Student.__init__(self)
+        self.position = ""
+        self.numEventsHelped = 0
+
+    def getPosition(self):
+        return position
+
+    def setPosition(self, position):
+        self.position = position
+
+    def getNumEventsHelped(self):
+        return self.getNumEventsHelped
+
+    def setNumEventsHelped(self, num):
+        self.numEventsHelped = num
+
 
 
 
